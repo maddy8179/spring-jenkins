e29adaed4a691b24f94f15bb9b6672a51e10e7db
maddy8179 <malreddy012@gmail.com>
2024-11-02 14:24:15 +0530

    second commit

--- spring-jenkins/src/test/java/com/jenkins/application/SpringJenkinsApplicationTests.java
+++ spring-jenkins/src/test/java/com/jenkins/application/SpringJenkinsApplicationTests.java
@@ -18,7 +18,7 @@ class SpringJenkinsApplicationTests {
 
 	@Test
 	void contextLoads() {
-		logger.info("Test case executing");
+		logger.info("Test case   executing");
 		assertEquals(true, true);
 	}
 
